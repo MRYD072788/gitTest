9b16978b9aaff6068b89b5a1b84aae92eefe82db
wyz <mty563699@gmail.com>
2023-08-09 15:37:41 +0800

    提交我是笨蛋

---
+++ src/main/java/com/wyz/gitTest/GitTest1.java
@@ -0,0 +1,16 @@
+package com.wyz.gitTest;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @author :wyz
+ * @description:
+ * @create :2023/8/9
+ */
+
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("我是笨蛋");
+    }
+}
